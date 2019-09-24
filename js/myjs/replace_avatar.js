cb33156d9933ed48f9462aa923727b57030a1d85
night-bright <izhaoyuanjie@163.com>
2019-09-24 12:58:47 +0800

    Site updated: 2019-09-24 12:58:45

--- js/myjs/replace_avatar.js
+++ js/myjs/replace_avatar.js
@@ -1,2 +1,6 @@
-var touxiang = "{{ url_for( theme.avatar.url | default(theme.images + '/avatar.gif') ) }}";
-console.log(touxiang);
+var image_num_arr = [1,2,3,4,5,6,7,8,9];
+var image_num = image_num_arr[Math.floor((Math.random()*image_num_arr.length))];
+var image_url = "/images/touxiang"+image_num+".jpg";
+$('.site-author-image').attr('src',image_url);
+//console.log(touxiang);
+
